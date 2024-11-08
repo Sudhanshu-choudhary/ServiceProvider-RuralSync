5e4f7a31a0b02effe04412337a0d6cb812bc75c3
Sudhanshu-choudhary <138672423+Sudhanshu-choudhary@users.noreply.github.com>
2024-11-08 20:11:47 +0530

    Delete src directory

--- src/constants.ts
+++
@@ -1,3 +0,0 @@
-export const AUTH_BASE_URL = "http://localhost:5001/auth/"
-export const SHOPKEEPER_BASE_URL = "http://localhost:5003/shopkeeper/";
-export const SERVICE_PROVIDER = "SERVICE_PROVIDER";
